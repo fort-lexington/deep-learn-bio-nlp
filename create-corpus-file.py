2fc003ac36d6eec61e932bb3899b89c760580033
Ryan M <fortlexington@gmail.com>
2019-08-24 15:33:03 -0700

    status info, better configuration

--- create-corpus-file.py
+++ create-corpus-file.py
@@ -1,9 +1,23 @@
 from corpus import DocumentCorpus
+import logging
+import sys
 
-docs = DocumentCorpus(root = r'/media/ryan/ExtraDrive1/PMC/XML/')
+status_inc = 10000
 
-with open('corpus.txt', 'w') as output:
-    for doc in docs:
-        text = ' '.join(doc)
-        output.write(text)
-        output.write("\n")
+def main(corpus_path, output_text):
+    docs = DocumentCorpus(root = corpus_path)
+
+    with open(output_text, 'w') as output:
+        for ix, doc in enumerate(docs):
+            text = ' '.join(doc)
+            output.write(text)
+            output.write("\n")
+            if ix % status_inc == 0:
+                logging.info('documents: {0}'.format(ix))
+    logging.info('DONE')
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    corpus_text = sys.argv[1]
+    main(r'/media/ryan/ExtraDrive1/PMC/XML/', 'corpus.txt')
+
